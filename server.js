59a752614c6e87cda4fd891a2be93ce3aa5f048c
Ross Libby <ross@frvctl.com>
2020-03-23 15:08:22 -0500

    post "here"

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import Discord from 'discord.js'
 import express from 'express'
+import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
 import axios from 'axios'
 dotenv.config()
@@ -7,10 +8,16 @@ dotenv.config()
 const {env: {DISCORD_TOKEN, PORT}} = process
 const client = new Discord.Client()
 
-const logAttendance = async () => {
+const logAttendance = async message => {
   const response = await axios({
     url: 'http://localhost:8080/log',
-    method: 'POST'
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    data: {
+      channelId: message.channel.id,
+      channel: message.channel.name,
+      user: message.author.username + '#' + message.author.discriminator
+    }
   }).then(async res => await res)
   .catch(async err => await err)
 
@@ -21,10 +28,14 @@ const logAttendance = async () => {
  * Express
  */
 const app = express()
-app.post('/log', (req, res) => res.send('Logged in style'))
+app.use(bodyParser.json())
+app.post('/log', (req, res) => {
+  console.log(req.body)
+  res.send('Logged in style')
+})
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
 client.on('ready', () => console.log(`Loggedin as ${client.user.tag}!`))
 
-client.on('message', async msg => msg.content === 'here' ? console.log(await logAttendance()) : null)
+client.on('message', async msg => msg.content === 'here' ? console.log(await logAttendance(msg)) : null)
 client.login(DISCORD_TOKEN)
